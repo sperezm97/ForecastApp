6bb3f137f96901d7cd24b0b8308f6c48e22534f8
Sebastian Perez <sperezm97@gmail.com>
2020-02-15 14:29:57 -0400

    Configure Navigation screen witj 2 examples screens

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as screen from "./src/screens";
 
 const ForeCastStack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <ForeCastStack.Navigator>
-        <ForeCastStack.screen />
+      <ForeCastStack.Navigator initialRouteName="home">
+        <ForeCastStack.screen name="home" component={screen.Home} />
+        <ForeCastStack.screen name="details" component={screen.Details} />
       </ForeCastStack.Navigator>
     </NavigationContainer>
   );
