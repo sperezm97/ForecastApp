17ae0e5bf1bde385d7b0896e4aafcec4f3696f67
Sebastian Perez <sperezm97@gmail.com>
2020-02-15 14:34:52 -0400

    Install React Native Eslint Style Guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ["plugin:react/recommended", "airbnb", "prettier", "react-native"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
